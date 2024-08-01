250df43d59146fc885d88aba529e3d51682809c9
Poliih <polianarodriguesds@gmail.com>
2024-08-01 17:06:20 -0300

    end

--- dist/routes/dataRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const dataController_1 = require("../controllers/dataController");
-const router = (0, express_1.Router)();
-router.get('/data/:chartType', dataController_1.getData);
-exports.default = router;
